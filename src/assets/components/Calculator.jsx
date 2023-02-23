733f2789c26551b142273ec24f7509eb0fc7eec5
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-23 15:30:52 +0100

    state

--- src/assets/components/Calculator.jsx
+++ src/assets/components/Calculator.jsx
@@ -5,19 +5,18 @@ const Calculator = () => {
     const [balance,changeBalance] = useState(0)
     const [value,changeValue] = useState(0)
 
-    
-
     return ( 
         <section>
             <h2>Balance</h2>
             <div>
                 <p>{balance}</p>
             </div>
-            <input className="border-2 border-black mb-4" type="text" name="" id="" />
+            <input onChange={(e) => {changeValue(e.target.value)}} className="border-2 border-black mb-4" type="text" name="" id="" />
             <div className="flex w-44 justify-between">
-                <button className="border-2 border-black">Deposit</button>
-                <button className="border-2 border-black">Withdrawal</button>
+                <button onClick={() => {changeBalance(balance + value)}} className="border-2 border-black">Deposit</button>
+                <button onClick={() => {changeBalance(balance - value)}} className="border-2 border-black">Withdrawal</button>
             </div>
+            <p>{value}</p>
         </section>
     );
 }
