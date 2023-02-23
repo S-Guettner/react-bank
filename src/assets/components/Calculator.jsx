5364497f0cb4ec7b75a8a919ed398646cd57125a
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-23 16:12:00 +0100

    changed input to number input

--- src/assets/components/Calculator.jsx
+++ src/assets/components/Calculator.jsx
@@ -5,6 +5,7 @@ const Calculator = () => {
     const [balance,changeBalance] = useState(0)
     const [value,changeValue] = useState(0)
 
+    
 
     return ( 
         <section className=" w-60 p-4 mx-auto border-2 border-black">
@@ -12,7 +13,7 @@ const Calculator = () => {
             <div>
                 <p>{balance}€</p>
             </div>
-            <input onChange={(e) => {changeValue(e.target.value)}} className="border-2 border-black mb-4" type="text" name="" id="" />
+            <input onChange={(e) => {changeValue(e.target.value)}} className="border-2 border-black mb-4" type="number" name="" id="" />
             <div className="flex w-44 justify-between">
                 <button onClick={() => {changeBalance(Number(balance) + Number(value))}} className="p-1 border-2 border-black">Deposit</button>
                 <button onClick={() => {changeBalance(Number(balance) - Number(value))}} className="p-1 border-2 border-black">Withdrawal</button>
